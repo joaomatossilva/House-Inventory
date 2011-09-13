43b04958c252e488cc95e9101dd47dc0f8c22354
Joao Silva <kappy@acydburne.com.pt>
2011-09-13 01:41:34 -0700

    added default rails skeleton

---
+++ HomeInventory/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HomeInventory::Application.config.secret_token = 'cbd52fb8b46b321413cf1d9fc6a4422f62573b562c82e7ba8db8a1a654570c3e198717eff32efdaea1493c8c4710b57f7aa6fbf6cb6eb0dcb34031780a5aaa78'
